51cf020324bc553a320687cb1634c6a1831d6a71
shashank17 <shashank.avusali@gmail.com>
2017-03-18 23:00:21 -0500

    removed unwanted files

--- BIP/HW2-part2/FindNeighbors.m
+++
@@ -1,7 +0,0 @@
-function [ Nlist ] = FindNeighbors( data, y, R)
-%   Finds the neighbors of a given data points that are with in a distance
-%   of R
-dists = pdist2(data,y);
-Nlist = dists < R;
-end
-
